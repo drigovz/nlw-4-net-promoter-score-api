4f542d4232ea68d6693e2ec497fc7a930b5c10dd
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-02-24 20:18:24 -0300

    feat: add show all surveys method

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 
 const router = Router(),
-    userController = new UserController();
+    userController = new UserController(),
+    surveysController = new SurveysController();
 
 router.post("/users", userController.create);
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
