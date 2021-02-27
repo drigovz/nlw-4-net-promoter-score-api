0ffe2760a990e118241141c9c2de9c07610751fd
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-02-27 14:28:27 -0300

    feat: add SendMail controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { SurveysController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router(),
     userController = new UserController(),
-    surveysController = new SurveysController();
+    surveysController = new SurveysController(),
+    sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
