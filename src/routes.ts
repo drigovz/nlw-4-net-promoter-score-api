3d6133cf51de607f95c0f5c23ec91747a57aad50
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-02-28 14:48:35 -0300

    feat: add answers funciton to vote

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,18 @@ import { Router } from 'express';
 import { SurveysController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
 
 const router = Router(),
     userController = new UserController(),
     surveysController = new SurveysController(),
-    sendMailController = new SendMailController();
+    sendMailController = new SendMailController(),
+    answerController = new AnswerController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 router.post("/sendMail", sendMailController.execute);
+router.get("/answers/:value", answerController.execute);
 
 export { router };
