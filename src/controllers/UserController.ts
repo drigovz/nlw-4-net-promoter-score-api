c1c00fb0ebdd577d8becd325b7fa7646a788297f
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-03-01 15:34:03 -0300

    refactor:! add validations on UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
 
 class UserController {
     async create(req: Request, res: Response) {
         const { name, email } = req.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Name is required"),
+            email: yup.string().email("E-mail not valid").required("E-mail is required")
+        });
+
+        try {
+            await schema.validate(req.body, { abortEarly: false });
+        } catch (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+
         const userRepository = getCustomRepository(UserRepository);
-        
+
         const user = userRepository.create({
             name,
             email
